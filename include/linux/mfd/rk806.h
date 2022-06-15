6137d211d98d3b5223f7c16cf7294d65c4cb6412
shengfei Xu <xsf@rock-chips.com>
2022-06-15 06:31:51 +0000

    mfd: rk806: adjust the rk806 spi write protocol
    To slove the error:
    the communication(spi-write) has a half-level problem.
    
    Signed-off-by: shengfei Xu <xsf@rock-chips.com>
    Change-Id: I93af4cb501d9076f5d5d9a0a3605406e2dc4b1c4

--- include/linux/mfd/rk806.h
+++ include/linux/mfd/rk806.h
@@ -276,7 +276,8 @@
 #define RK806_CMD_LEN_MSK		0x0f
 #define RK806_REG_H			0x00
 
-#define VERSION_AB		0x01
+#define VERSION_AB			0x01
+#define RK806_READ_BUFFER_SIZE         32
 
 enum rk806_reg_id {
 	RK806_ID_DCDC1 = 0,
@@ -516,6 +517,7 @@ struct rk806 {
 	struct regmap_irq_chip_data *irq_data;
 	struct rk806_platform_data *pdata;
 	struct rk806_pin_info *pins;
+	u8 read_buf[RK806_READ_BUFFER_SIZE];
 	int vb_lo_irq;
 };
 
