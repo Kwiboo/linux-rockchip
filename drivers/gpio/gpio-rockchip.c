be1f080ebf90cd99ac67fa5d95866a6c20411af5
Ye Zhang <ye.zhang@rock-chips.com>
2024-08-09 10:05:21 +0800

    gpio: rockchip: Prevent underflow unsigned variables.
    Signed-off-by: Ye Zhang <ye.zhang@rock-chips.com>
    Change-Id: I2910fe7c57683bbd175cbc4b28584ff84f037d07

--- drivers/gpio/gpio-rockchip.c
+++ drivers/gpio/gpio-rockchip.c
@@ -200,8 +200,8 @@ static int rockchip_gpio_set_debounce(struct gpio_chip *gc,
 	unsigned int cur_div_reg;
 	u64 div;
 
-	if (bank->gpio_type == GPIO_TYPE_V2 && !IS_ERR(bank->db_clk)) {
-		div_debounce_support = true;
+	div_debounce_support = (bank->gpio_type == GPIO_TYPE_V2) && !IS_ERR(bank->db_clk);
+	if (debounce && div_debounce_support) {
 		freq = clk_get_rate(bank->db_clk);
 		if (!freq)
 			return -EINVAL;
@@ -211,8 +211,6 @@ static int rockchip_gpio_set_debounce(struct gpio_chip *gc,
 
 		div = debounce * freq;
 		div_reg = DIV_ROUND_CLOSEST_ULL(div, 2 * USEC_PER_SEC) - 1;
-	} else {
-		div_debounce_support = false;
 	}
 
 	raw_spin_lock_irqsave(&bank->slock, flags);
