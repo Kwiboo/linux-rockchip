fc76ab1e22c39f56a2491eda429f935b7b995c6d
Elaine Zhang <zhangqing@rock-chips.com>
2025-05-08 09:16:32 +0800

    clk: fractional-divider: Fix compilation errors
    drivers/clk/clk-fractional-divider.c: In function ‘clk_fd_set_rate’:
    drivers/clk/clk-fractional-divider.c:217:13: error: ‘struct
    clk_fractional_divider’ has no member named ‘nmask’
       if (n > fd->nmask) {
                 ^~
    drivers/clk/clk-fractional-divider.c:220:10: error: ‘struct
    clk_fractional_divider’ has no member named ‘nmask’
        n = fd->nmask;
    
    Change-Id: Ia114e7f7d00e966a496fca2c144e6cc382082b27
    Signed-off-by: Elaine Zhang <zhangqing@rock-chips.com>

--- drivers/clk/clk-fractional-divider.c
+++ drivers/clk/clk-fractional-divider.c
@@ -181,6 +181,10 @@ static int clk_fd_set_rate(struct clk_hw *hw, unsigned long rate,
 		m--;
 		n--;
 	}
+
+	mmask = GENMASK(fd->mwidth - 1, 0) << fd->mshift;
+	nmask = GENMASK(fd->nwidth - 1, 0) << fd->nshift;
+
 	/*
 	 * When compensation the fractional divider,
 	 * the [1:0] bits of the numerator register are omitted,
@@ -201,10 +205,10 @@ static int clk_fd_set_rate(struct clk_hw *hw, unsigned long rate,
 
 		n *= val;
 		m *= val;
-		if (n > fd->nmask) {
+		if (n > nmask) {
 			pr_debug("%s n(%ld) is overflow, use mask value\n",
 				 __func__, n);
-			n = fd->nmask;
+			n = nmask;
 		}
 	}
 
