e94e3223abaa3a79ba3efc4e9e0359738109635a
William Wu <william.wu@rock-chips.com>
2025-06-09 11:20:45 +0800

    Revert "FROMLIST: usb: xhci: account for num_trbs_free when invalidating TDs"
    This reverts commit 666bac0de5468ec47455f15d58301180d755dafd.
    
    Change-Id: I2eb6c75410ca797f637db714d1245c4a64ade981
    Signed-off-by: William Wu <william.wu@rock-chips.com>

--- drivers/usb/host/xhci-ring.c
+++ drivers/usb/host/xhci-ring.c
@@ -1028,13 +1028,11 @@ static int xhci_invalidate_cancelled_tds(struct xhci_virt_ep *ep)
 						 td->urb->stream_id, td->urb,
 						 cached_td->urb->stream_id, cached_td->urb);
 				cached_td = td;
-				ring->num_trbs_free += td->num_trbs;
 				break;
 			}
 		} else {
 			td_to_noop(xhci, ring, td, false);
 			td->cancel_status = TD_CLEARED;
-			ring->num_trbs_free += td->num_trbs;
 		}
 	}
 
