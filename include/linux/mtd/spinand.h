3eb938ce99233552ab2924fff7686c742ecba1c4
Jon Lin <jon.lin@rock-chips.com>
2025-08-05 14:47:13 +0800

    BACKPORT: mtd: spinand: add support for ESMT F50x1G41LB
    This patch adds support for ESMT F50L1G41LB and F50D1G41LB. It seems
    that ESMT likes to use random JEDEC ID from other vendors. Their 1G
    chips uses 0xc8 from GigaDevice and 2G/4G chips uses 0x2c from Micron.
    For this reason, the ESMT entry is named esmt_c8 with explicit JEDEC
    ID in variable name.
    
    Datasheets:
    https://www.esmt.com.tw/upload/pdf/ESMT/datasheets/F50L1G41LB(2M).pdf
    https://www.esmt.com.tw/upload/pdf/ESMT/datasheets/F50D1G41LB(2M).pdf
    
    Change-Id: I3fbefb336e279a31a7fcef50680541746c18c7ba
    Signed-off-by: Chuanhong Guo <gch981213@gmail.com>
    Signed-off-by: Martin Kurbanov <mmkurbanov@sberdevices.ru>
    Signed-off-by: Dmitry Rokosov <ddrokosov@sberdevices.ru>
    Tested-by: Martin Kurbanov <mmkurbanov@sberdevices.ru>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Link: https://lore.kernel.org/linux-mtd/20230329114240.378722-1-mmkurbanov@sberdevices.ru
    [jon: Resolved conflict at esmt.c]
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- include/linux/mtd/spinand.h
+++ include/linux/mtd/spinand.h
@@ -241,7 +241,7 @@ struct spinand_manufacturer {
 /* SPI NAND manufacturers */
 extern const struct spinand_manufacturer biwin_spinand_manufacturer;
 extern const struct spinand_manufacturer dosilicon_spinand_manufacturer;
-extern const struct spinand_manufacturer esmt_spinand_manufacturer;
+extern const struct spinand_manufacturer esmt_c8_spinand_manufacturer;
 extern const struct spinand_manufacturer etron_spinand_manufacturer;
 extern const struct spinand_manufacturer fmsh_spinand_manufacturer;
 extern const struct spinand_manufacturer foresee_spinand_manufacturer;
