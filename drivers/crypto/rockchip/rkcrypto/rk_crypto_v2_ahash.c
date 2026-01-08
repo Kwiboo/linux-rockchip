5abb702073915bddc4e6989f9179ddafb4bd862d
Lin Jinhan <troy.lin@rock-chips.com>
2026-01-08 01:19:02 +0000

    crypto: rockchip: sync kernel 6.12 support
    Change-Id: I9cde9eeb6a04658d130f41153aa5bef8599b29e3
    Signed-off-by: Lin Jinhan <troy.lin@rock-chips.com>

--- drivers/crypto/rockchip/rkcrypto/rk_crypto_v2_ahash.c
+++ drivers/crypto/rockchip/rkcrypto/rk_crypto_v2_ahash.c
@@ -130,7 +130,7 @@ static void rk_ahash_crypto_complete(struct crypto_async_request *base, int err)
 	}
 
 	if (base->complete)
-		base->complete(base, err);
+		base->complete(COMPLETE_PARAM(base), err);
 }
 
 static int write_key_reg(struct rk_crypto_dev *rk_dev, const u8 *key,
