a95cce5683d4c75902af5a9b7509caad1665af30
Shawn Lin <shawn.lin@rock-chips.com>
2026-02-27 15:16:22 +0800

    phy: rockchip: naneng-combphy: Fix ssc module period frequency
    For some instances of Naneng combo PHYs, the SSC module period frequency
    is not automatically adjusted by the PLL reference clock. Therefore,
    enable the PLL-controlled SSC module period bit and set the correct
    ssc_cnt for a 100MHz/24MHz reference clock.
    
    This commit affects RK3588(s)/RK3576/RK3572.
    
    Change-Id: Ie339eca0f62c460d6c6d4ce8d7879a44de9fd1d1
    Signed-off-by: Shawn Lin <shawn.lin@rock-chips.com>

--- drivers/phy/rockchip/phy-rockchip-naneng-combphy.c
+++ drivers/phy/rockchip/phy-rockchip-naneng-combphy.c
@@ -1147,6 +1147,8 @@ static int rk3572_combphy_cfg(struct rockchip_combphy_priv *priv)
 
 	if (device_property_read_bool(priv->dev, "rockchip,enable-ssc")) {
 		rockchip_combphy_updatel(priv, GENMASK(4, 4), BIT(4), 0x7 << 2);
+		/* PLL control SSC module period should be set */
+		rockchip_combphy_updatel(priv, GENMASK(5, 5), BIT(5), 0x20 << 2);
 
 		if (priv->mode == PHY_TYPE_PCIE && rate == 24000000) {
 			/* Xin24M T0_1 650mV */
@@ -1160,6 +1162,13 @@ static int rk3572_combphy_cfg(struct rockchip_combphy_priv *priv)
 			writel(0x40, priv->mmio + (0x0e << 2));
 			writel(0x5f, priv->mmio + (0x0f << 2));
 			writel(0x10, priv->mmio + (0x20 << 2));
+			/* ssc_cnt: 0101111101 & 31.5KHz */
+			rockchip_combphy_updatel(priv, GENMASK(7, 6), 0x1, 0xe << 2);
+			rockchip_combphy_updatel(priv, GENMASK(7, 0), 0x5f, 0xf << 2);
+		} else if (priv->mode == PHY_TYPE_PCIE && rate == 100000000) {
+			/* ssc_cnt: 1000110001 & 31.5KHz */
+			rockchip_combphy_updatel(priv, GENMASK(7, 6), 0x1, 0xe << 2);
+			rockchip_combphy_updatel(priv, GENMASK(7, 0), 0x8c, 0xf << 2);
 		}
 	}
 
@@ -1367,6 +1376,8 @@ static int rk3576_combphy_cfg(struct rockchip_combphy_priv *priv)
 
 	if (device_property_read_bool(priv->dev, "rockchip,enable-ssc")) {
 		rockchip_combphy_updatel(priv, GENMASK(4, 4), BIT(4), 0x7 << 2);
+		/* PLL control SSC module period should be set */
+		rockchip_combphy_updatel(priv, GENMASK(5, 5), BIT(5), 0x20 << 2);
 
 		if (priv->mode == PHY_TYPE_PCIE && rate == 24000000) {
 			/* Xin24M T0_1 650mV */
@@ -1380,6 +1391,13 @@ static int rk3576_combphy_cfg(struct rockchip_combphy_priv *priv)
 			writel(0x40, priv->mmio + (0x0e << 2));
 			writel(0x5f, priv->mmio + (0x0f << 2));
 			writel(0x10, priv->mmio + (0x20 << 2));
+			/* ssc_cnt: 0101111101 & 31.5KHz */
+			rockchip_combphy_updatel(priv, GENMASK(7, 6), 0x1, 0xe << 2);
+			rockchip_combphy_updatel(priv, GENMASK(7, 0), 0x5f, 0xf << 2);
+		} else if (priv->mode == PHY_TYPE_PCIE && rate == 100000000) {
+			/* ssc_cnt: 1000110001 & 31.5KHz */
+			rockchip_combphy_updatel(priv, GENMASK(7, 6), 0x1, 0xe << 2);
+			rockchip_combphy_updatel(priv, GENMASK(7, 0), 0x8c, 0xf << 2);
 		}
 	}
 
@@ -1607,6 +1625,8 @@ static int rk3588_combphy_cfg(struct rockchip_combphy_priv *priv)
 
 	if (device_property_read_bool(priv->dev, "rockchip,enable-ssc")) {
 		rockchip_combphy_updatel(priv, GENMASK(4, 4), BIT(4), 0x7 << 2);
+		/* PLL control SSC module period should be set */
+		rockchip_combphy_updatel(priv, GENMASK(5, 5), BIT(5), 0x20 << 2);
 
 		if (priv->mode == PHY_TYPE_PCIE && rate == 24000000) {
 			/* Xin24M T0_1 650mV */
@@ -1620,6 +1640,13 @@ static int rk3588_combphy_cfg(struct rockchip_combphy_priv *priv)
 			writel(0x40, priv->mmio + (0x0e << 2));
 			writel(0x5f, priv->mmio + (0x0f << 2));
 			writel(0x10, priv->mmio + (0x20 << 2));
+			/* ssc_cnt: 0101111101 & 31.5KHz */
+			rockchip_combphy_updatel(priv, GENMASK(7, 6), 0x1, 0xe << 2);
+			rockchip_combphy_updatel(priv, GENMASK(7, 0), 0x5f, 0xf << 2);
+		} else if (priv->mode == PHY_TYPE_PCIE && rate == 100000000) {
+			/* ssc_cnt: 1000110001 & 31.5KHz */
+			rockchip_combphy_updatel(priv, GENMASK(7, 6), 0x1, 0xe << 2);
+			rockchip_combphy_updatel(priv, GENMASK(7, 0), 0x8c, 0xf << 2);
 		}
 	}
 
