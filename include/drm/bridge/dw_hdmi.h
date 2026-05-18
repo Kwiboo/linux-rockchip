ad5fe4ecb2dc95ce909ab836047b04fa10a8a8a1
Jonas Karlman <jonas@kwiboo.se>
2026-05-18 18:01:55 +0000

    drm: bridge: dw_hdmi: Use delayed_work to debounce hotplug event
    HDMI Specification Version 1.4b chapter 8.5 mentions:
    
      An HDMI Sink shall not assert high voltage level on its Hot Plug
      Detect pin when the E-EDID is not available for reading.
    
      A Source may use a high voltage level Hot Plug Detect signal to
      initiate the reading of E-EDID data.
    
      An HDMI Sink shall indicate any change to the contents of the E-EDID
      by driving a low voltage level pulse on the Hot Plug Detect pin. This
      pulse shall be at least 100 msec.
    
    Use a delayed work to debounce reacting on HPD events to improve
    handling of a HPD low voltage level pulse when a sink changes the EDID.
    
    The delayed work is only enabled between enable_hpd()/hpd_enable() and
    disable_hpd()/hpd_disable() calls from core, i.e. enabled after
    attach/bind/resume and disabled before detach/unbind/suspend.
    
    The 1100 msec hotplug debounce timeout was arbitrarily picked to match
    other drivers using same const, and testing using a Raspberry Pi Monitor
    seem to use a 200-300 msec pulse when going from standby to power on
    state.
    
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

--- include/drm/bridge/dw_hdmi.h
+++ include/drm/bridge/dw_hdmi.h
@@ -186,6 +186,8 @@ struct dw_hdmi *dw_hdmi_bind(struct platform_device *pdev,
 
 void dw_hdmi_resume(struct dw_hdmi *hdmi);
 
+void dw_hdmi_schedule_hpd_work(struct dw_hdmi *hdmi,
+			       enum drm_connector_status status);
 void dw_hdmi_setup_rx_sense(struct dw_hdmi *hdmi, bool hpd, bool rx_sense);
 
 int dw_hdmi_set_plugged_cb(struct dw_hdmi *hdmi, hdmi_codec_plugged_cb fn,
