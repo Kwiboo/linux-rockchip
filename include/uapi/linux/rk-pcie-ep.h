e382ee44a5f7cb1ce7a76ad308380ef0feb5f820
Jon Lin <jon.lin@rock-chips.com>
2026-08-05 10:30:58 +0800

    uapi: rk-pcie-ep: update SPDX license to (GPL-2.0 WITH Linux-syscall-note) OR MIT
    Change the SPDX license identifier of include/uapi/linux/rk-pcie-ep.h
    from "GPL-2.0 WITH Linux-syscall-note" to
    "((GPL-2.0 WITH Linux-syscall-note) OR MIT)".
    
    As this header is exported to userspace via uapi, adding the MIT dual
    license option allows non-GPL userspace projects to include it without
    GPL constraints. This aligns with the dual-licensing practice used by
    other kernel uapi headers.
    
    Change-Id: I7efef04aa2d4086920d8c6bbb0b10004393a1776
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- include/uapi/linux/rk-pcie-ep.h
+++ include/uapi/linux/rk-pcie-ep.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+/* SPDX-License-Identifier: ((GPL-2.0 WITH Linux-syscall-note) OR MIT) */
 /*
  * Copyright (c) 2022 Rockchip Electronics Co., Ltd.
  */
