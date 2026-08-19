95a3ad83f7f90e0e672b9c99b0858b4964318667
Jon Lin <jon.lin@rock-chips.com>
2026-08-19 18:28:01 +0800

    mtd: spi-nor: gigadevice: Support new device gd55lb01gf
    Change-Id: I85f5be0cae34c63d9c86212e3b6cfbe395206768
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/spi-nor/gigadevice.c
+++ drivers/mtd/spi-nor/gigadevice.c
@@ -79,6 +79,9 @@ static const struct flash_info gigadevice_parts[] = {
 	{ "gd25lt512m", INFO(0xc8661a, 0, 64 * 1024, 1024,
 			     SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
 			     SPI_NOR_4B_OPCODES | SPI_NOR_HAS_LOCK) },
+	{ "gd55lb01gf", INFO(0xc8601b, 0, 64 * 1024, 2048,
+			     SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
+			     SPI_NOR_4B_OPCODES | SPI_NOR_HAS_LOCK) },
 };
 
 const struct spi_nor_manufacturer spi_nor_gigadevice = {
